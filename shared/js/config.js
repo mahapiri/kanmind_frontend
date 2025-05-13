9d93319aef9128d0621d271f19ad1fccc71757ce
Pirathib Mahalingam <pirathib.m@gmail.com>
2025-05-13 13:22:27 +0200

    edited guest profile

--- shared/js/config.js
+++ shared/js/config.js
@@ -1,6 +1,6 @@
 const GUEST_LOGIN = {
-    "email": "kevin@kovacsi.de",
-    "password": "asdasdasd"
+    "email": "gast@tmail.de",
+    "password": "Hallo123"
 }
 
 const API_BASE_URL = 'http://127.0.0.1:8000/api/';
